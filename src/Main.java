18513cc1add01d543de84691250b32779ea5ffc8
Emma Parisgit config --global user.name Skream <tumho00005@gmail.com>
2023-08-02 19:38:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Comparator<Integer> comparator = (x, v) -> {
+            if (x > v) return 1;
+            else if (x < v) return -1;
+            else return 0;
+        };
+
+        TreeSet<Integer> intTree = new TreeSet<>(comparator);
+        intTree.addAll(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
+        Iterator<Integer> iterator = intTree.iterator();
+        while (iterator.hasNext()) {
+            int element = iterator.next();
+            if (element % 2 != 0 || element < 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println(intTree);
+    }
+}
